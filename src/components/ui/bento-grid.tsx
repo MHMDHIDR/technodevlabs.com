ec6d71d7f8c6c43a75b8c4fb49651698f9e133a2
Mohammed Haydar <mr.hamood277@gmail.com>
2024-09-28 18:23:01 +0100

    Refactor BentoGridItem component styles

--- src/components/ui/bento-grid.tsx
+++ src/components/ui/bento-grid.tsx
@@ -58,12 +58,12 @@ export const BentoGridItem = ({
   return (
     <div
       className={cn(
-        'row-span-1 rounded-xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4 dark:bg-black dark:border-white/[0.2] bg-white border border-transparent justify-between flex flex-col space-y-4',
+        'row-span-1 rounded-xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none relative dark:bg-black dark:border-white/[0.2] bg-white border border-transparent justify-between flex flex-col gap-y-2',
         className
       )}
     >
       {header}
-      <div className='group-hover/bento:translate-x-2 transition duration-200'>
+      <div className='group-hover/bento:translate-x-2 transition duration-200 absolute bottom-0 bg-black'>
         {icon}
         <div className='font-bold text-neutral-600 dark:text-neutral-200 mb-2 mt-2'>
           {title}
