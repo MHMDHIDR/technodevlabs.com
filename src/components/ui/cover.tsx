d16db48151bc3126fee6b4974aca722c905e97f4
Mohammed Haydar <mr.hamood277@gmail.com>
2024-10-05 06:23:38 +0100

    Refactor: Update className in Cover component to include 'select-none'

--- src/components/ui/cover.tsx
+++ src/components/ui/cover.tsx
@@ -37,7 +37,7 @@ export const Cover = ({
       onMouseEnter={() => setHovered(true)}
       onMouseLeave={() => setHovered(false)}
       ref={ref}
-      className='relative inline-block px-4 py-2 rounded-sm transition duration-200 hover:bg-neutral-900 group/cover dark:bg-neutral-900 bg-neutral-100'
+      className='relative select-none inline-block px-4 py-2 rounded-sm transition duration-200 hover:bg-neutral-900 group/cover dark:bg-neutral-900 bg-neutral-100'
     >
       <AnimatePresence>
         {hovered && (
