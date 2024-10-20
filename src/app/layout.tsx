a240404729a8fc38c3f9213f0d001f2ef2bafc07
Mohammed Haydar <mr.hamood277@gmail.com>
2024-10-21 00:37:52 +0100

    fixes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,10 @@ import './globals.css'
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html>
+      <head>
+        <meta content='width=device-width, initial-scale=1 maximum-scale=1' name='viewport' />
+        <link href='/images/logo.svg' rel='icon' type='image/svg+xml' />
+      </head>
       <body>{children}</body>
     </html>
   )
