5d57aa6107661f5cca2cd656308014c8181e552e
Mohammed Haydar <mr.hamood277@gmail.com>
2024-12-04 12:12:46 +0000

    Refactor CardsCarousel component: Remove commented out code for backdrop blur effect

--- src/components/ui/cards-carousel.tsx
+++ src/components/ui/cards-carousel.tsx
@@ -312,12 +312,12 @@ const ExpandedCard = ({
 
   return (
     <div className='overflow-auto fixed inset-0 z-50 h-screen'>
-      {/* <motion.div
+      <motion.div
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         exit={{ opacity: 0 }}
         className='fixed inset-0 w-full h-full backdrop-blur-lg bg-black/80'
-      /> */}
+      />
       <motion.div
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
