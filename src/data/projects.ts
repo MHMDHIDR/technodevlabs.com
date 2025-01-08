9c584c2a50d81751dad6ff130a2e2a58c89f114c
Mohammed Haydar <mr.hamood277@gmail.com>
2025-01-08 02:55:12 +0000

    showing the latest added projects in order

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,27 +1,30 @@
+import { desc, sql } from 'drizzle-orm'
 import { getBlurPlaceholder } from '@/actions'
 import { database } from '@/db/database'
+import { projects } from '@/db/schema'
 import type { ProjectWithBlur } from '@/types'
 
 /**
- * Get all projects and their count
+ * Get all projects and their count, ordered by updatedAt descending
  * @returns {Promise<{ projects: Array<ProjectWithBlur>; projectsCount: number }>} A promise that contains an array of projects and their count
  */
 export async function getProjects(): Promise<{
   projects: Array<ProjectWithBlur>
   projectsCount: number
 }> {
-  const projects = await database.query.projects.findMany()
+  const projectsList = await database.select().from(projects).orderBy(desc(projects.updatedAt))
+  const [{ count }] = await database.select({ count: sql`count(*)` }).from(projects)
+  const projectsCount = Number(count)
 
-  if (!projects || projects.length === 0) {
+  if (!projectsList || projectsList.length === 0) {
     return { projects: [], projectsCount: 0 }
   }
 
   const projectsWithBlur = await Promise.all(
-    projects.map(async project => {
+    projectsList.map(async project => {
       const imagesWithBlur = await Promise.all(
         project.images.map(async imageSrc => {
           const blurDataURL = await getBlurPlaceholder(imageSrc)
-
           return { src: imageSrc, blurDataURL: blurDataURL || '' }
         })
       )
@@ -29,5 +32,5 @@ export async function getProjects(): Promise<{
     })
   )
 
-  return { projects: projectsWithBlur, projectsCount: projects.length }
+  return { projects: projectsWithBlur, projectsCount }
 }
